438767b55aaa8ab45699fe0c59a50989bb53aa4d
carolineglass <carolineglass89@gmail.com>
2020-04-22 13:55:00 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
 
+class CashRegister 
+    attr_accessor :total
+    attr_accessor :discount
+    attr_reader :apply_discount
+
+    def initialize(discount = 0)
+        @discount = discount
+        @total = 0
+    end 
+    def add_item(title, price, quantity = 1)
+        @total += price * quantity
+    end
+    def apply_discount
+        # it 'applies the discount to the total price' do
+        #     cash_register_with_discount.add_item("macbook air", 1000)
+        #     cash_register_with_discount.apply_discount
+        #     expect(cash_register_with_discount.total).to eq(800)
+        discount_amount = @total * @discount/100
+        total_after_discount = @total - discount_amount
+        total_after_discount
+    end
+end 
+
